9e83d8e7bdde2f1fd184d61074a90af67f75361c
Dina Shouhdy <dshouhdy@gmail.com>
2024-06-08 11:36:34 -0700

    created components, pages, and setup routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Homepage from "./pages/Homepage/Homepage";
+import ItemList from "./pages/ItemList/ItemList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+        <Header />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/ItemList" element={<ItemList />} />
+          </Routes>
+        <Footer />
+    </Router>
   );
 }
 
