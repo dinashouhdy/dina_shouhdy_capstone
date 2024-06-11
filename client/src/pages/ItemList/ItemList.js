c47847d35e8e8648776dd945e43db5c4fb2c1328
Dina Shouhdy <dshouhdy@gmail.com>
2024-06-10 18:12:28 -0700

    started frontend itemList

--- client/src/pages/ItemList/ItemList.js
+++ client/src/pages/ItemList/ItemList.js
@@ -1,25 +1,73 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-// import './ItemList.scss';
+import { useEffect } from 'react';
+import './ItemList.scss';
 
 function ItemList() {
+
+  const [leftItems, setLeftItems] = useState([]);
+  const [rightItems, setRightItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/ItemList')
+      .then(response => {
+        setLeftItems(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching items:', error);
+      });
+  }, []);
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  const handleMoveToRight = () => {
+    if (selectedItem) {
+      setLeftItems(leftItems.filter(item => item !== selectedItem));
+      setRightItems([...rightItems, selectedItem]);
+      setSelectedItem(null);
+    }
+  };
+
+  const handleMoveToLeft = () => {
+    if (selectedItem) {
+      setRightItems(rightItems.filter(item => item !== selectedItem));
+      setLeftItems([...leftItems, selectedItem]);
+      setSelectedItem(null);
+    }
+  };
+
   return (
     <section className="item-list">
       <div className="item-list__container">
-        <div className="item-list__left">
+        <div className="item-list__left item-list__box">
           <h2>Left List</h2>
           <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
+            {leftItems.map((item, index) => (
+              <li 
+                key={index} 
+                onClick={() => handleItemClick(item)}
+                className={selectedItem === item ? 'selected' : ''}
+              >
+                {item}
+              </li>
+            ))}
           </ul>
         </div>
-        <div className="item-list__right">
+        <div className="item-list__buttons">
+          <button onClick={handleMoveToRight}>&rarr;</button>
+          <button onClick={handleMoveToLeft}>&larr;</button>
+        </div>
+        <div className="item-list__right item-list__box">
           <h2>Right List</h2>
           <ul>
-            <li>Item A</li>
-            <li>Item B</li>
-            <li>Item C</li>
+            {rightItems.map((item, index) => (
+              <li key={index}>
+                {item}
+              </li>
+            ))}
           </ul>
         </div>
       </div>
